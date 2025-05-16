c3dc896c2e43f787037e662ebe0d7aa3e33ee4ab
jiakun93 <156577404+jiakun93@users.noreply.github.com>
2025-05-17 02:01:16 +0800

    Update layout.tsx
    默认主题dark to system

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -61,7 +61,7 @@ export default async function RootLayout({
         <NextIntlClientProvider locale={locale} messages={messages}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
