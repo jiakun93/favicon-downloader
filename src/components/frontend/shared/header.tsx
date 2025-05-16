642c8e60ef2c5fb4a3ff4436d73918534c2d7e33
jiakun93 <156577404+jiakun93@users.noreply.github.com>
2025-05-17 01:58:48 +0800

    Update header.tsx

--- src/components/frontend/shared/header.tsx
+++ src/components/frontend/shared/header.tsx
@@ -32,11 +32,11 @@ export function Header() {
   <header className={cn(" w-full items-center gap-4  bg-background px-4 md:px-6 z-50 h-16 transition-shadow duration-200 flex", isTop ? "shadow-none" : "shadow-sm")}>
     <NavBar />
     <div className="flex items-center justify-end gap-2 md:ml-auto text-primary">
-      <a href="https://www.buymeacoffee.com/seadfeng" target="_blank" title="Buy Me A Coffee"><img className="rounded-full" width={25} height={25} src="https://www.faviconextractor.com/favicon/buymeacoffee.com?larger=true" alt="Buy Me A Coffee" /> </a>
+      <a href="https://www.buymeacoffee.com/jiakun" target="_blank" title="Buy Me A Coffee"><img className="rounded-full" width={25} height={25} src="https://www.faviconextractor.com/favicon/buymeacoffee.com?larger=true" alt="Buy Me A Coffee" /> </a>
       <LocaleSwitch />
       <ModeToggle /> 
-      <a href="https://github.com/seadfeng/favicon-downloader" className="flex flex-shrink-0">
-        <img title="Favicon Downloader Github" alt="Favicon Downloader Github" height={20} className="h-[20px]" src="https://img.shields.io/github/stars/seadfeng/favicon-downloader?style=social" />
+      <a href="https://github.com/jiakun93/favicon-downloader" className="flex flex-shrink-0">
+        <img title="Favicon Downloader Github" alt="Favicon Downloader Github" height={20} className="h-[20px]" src="https://img.shields.io/github/stars/jiakun93/favicon-downloader?style=social" />
       </a>
     </div>
   </header>
